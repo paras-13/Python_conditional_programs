8b7ab37c106f0ce295401460926e8a5d54a2f77a
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-11-11 19:30:20 +0530

    Factorial of a number
    To find factorial of an number using loop and recursion.

--- Factorial.py
+++ Factorial.py
@@ -1,4 +1,16 @@
-# Factorial of a number
+#Factorial of a number
+number = int(input("Enter a number for which you want to find the factorial -->"))
+fact = 1
+for i in range(1,number+1):
+    fact*=i
+print(fact)
+
+print()
+print("------------------------------------------------")
+print("Using Recurrsion")
+print("---------------------------------------------")
+print()
+# Factorial of a number using recurssion
 a=1
 def factorial(num):
     global a
@@ -6,5 +18,5 @@ def factorial(num):
         a=a*num
         factorial(num-1)
     return a
-num = int(input())
+num = int(input("Enter a number for which you want to find the factorial -->"))
 print(factorial(num))
