41901dc479617d91ff7cb7a1a97aae9773214ea6
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-11-27 02:06:49 +0530

    Loop append

--- Loop_question.py
+++ Loop_question.py
@@ -1,8 +1,5 @@
 # Using a for loop and .append() method append each item with a Dr. prefix to the lst.
-
 list1=["Phil", "Oz", "Seuss", "Dre"]
 for i in range(len(list1)):   
     list1[i] = "Dr."+list1[i]                             #Lists are mutable
 print(list1)
-
-
