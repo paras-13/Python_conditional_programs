556b9c35f1abcc8f6eee4eed6dc990e04bf0b971
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-11-27 01:54:13 +0530

    Celsius to Fahrenheit converter

---
+++ Celsius_to_Fahrenheit.py
@@ -0,0 +1,5 @@
+# Convert celsius degree into fahrenheit
+
+celsius = float(input("Enter tempertaure in degree celsius"))
+fah = (9/5)*celsius + 32
+print(f"{celsius} celsius is {fah} in fahrenheit")
