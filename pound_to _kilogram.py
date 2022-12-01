ab2b66e8feb0e82f5d5345b58f0188790a04ff0b
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-12-01 20:35:01 +0530

    Pound to kilogram conversion

---
+++ pound_to _kilogram.py
@@ -0,0 +1,5 @@
+# Program to convert weight from pound to kilogram
+# 1 pound is 0.454 kg
+pound = float(input("Enter weight in pound"))
+in_kgs = pound*0.454
+print(f"{pound} pound is {in_kgs} kiloggram")
