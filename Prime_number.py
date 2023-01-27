aa5f952b8051e51ed3192501b1bac2e44b46dc47
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-01-28 02:07:07 +0530

    Prime numbers in a range

--- Prime_number.py
+++ Prime_number.py
@@ -2,10 +2,12 @@
 # Take the user input for start and end of range.
 start= int(input("Enter a number from where you want start the range"))
 end = int(input("Enter a number at where you want to end the range"))
+lst = []
 for i in range(start,end+1):
     if i>1:
         for j in range(2,i):
             if i%j == 0:
                 break
         else:
-            print(i,end = ",")
+            lst.append(i)
+print(lst)
